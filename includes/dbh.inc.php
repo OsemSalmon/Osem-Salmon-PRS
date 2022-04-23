b372c8959c179fcdd66f828a96fc402e1d85d863
zzulkar9 <muhdzzulkar9@gmail.com>
2022-04-23 15:49:33 +0800

    Backend Login

---
+++ includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$serverName = "localhost";
+$dbUserName = "root";
+$dbPassword = "";
+$dbName = "osemsalmon";
+
+$conn = mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);
+
+if (!$conn){
+    die("Connection Failed: " .mysqli_connect_error());
+}
