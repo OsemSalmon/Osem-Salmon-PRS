527724d162e0abb0d17253bc113589913beb6e5e
Ariff_Iskandar <58976797+IsAlp@users.noreply.github.com>
2022-05-26 21:04:54 +0800

    Revert "Update firebase config"
    This reverts commit 0ee550397ebfe55f2f9708c72e8cd7324fde9b40.

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { ref, onUnmounted } from 'vue'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const Config = {
+const firebaseConfig = {
   apiKey: "AIzaSyCo7JdP7m6pQ_EWOHM9U6FR1a1_1gYe4O0",
   authDomain: "vue-osem-salmon.firebaseapp.com",
   projectId: "vue-osem-salmon",
@@ -17,37 +16,7 @@ const Config = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(Config)
-const auth = getAuth(firebaseApp)
-
-//From Cruz 
-const db = firebaseApp.firestore()
-const usersCollection = db.collection('users')
-
-export const createUser = user => {
-  return usersCollection.add(user)
-}
-
-export const getUser = async id => {
-  const user = await usersCollection.doc(id).get()
-  return user.exists ? user.data() : null
-}
-
-export const updateUser = (id, user) => {
-  return usersCollection.doc(id).update(user)
-}
-
-export const deleteUser = id => {
-  return usersCollection.doc(id).delete()
-}
-
-export const useLoadUsers = () => {
-  const users = ref([])
-  const close = usersCollection.onSnapshot(snapshot => {
-    users.value = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-  })
-  onUnmounted(close)
-  return users
-}
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
 
 export { auth }
