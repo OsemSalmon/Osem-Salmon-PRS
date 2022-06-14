3092cef813055c9ba891cd7cf6b43f03964ec855
MUHAMAD ALIFF FAHMI BIN MOHAMAD ASRI <68102284+fahmi2000@users.noreply.github.com>
2022-06-14 08:30:24 +0800

    refactor firebase
    -connection to firebase
    -fetching collection

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,53 +1,42 @@
 
 import { initializeApp } from "firebase/app"
-import { getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth"
+import { getFirestore, collection, getDocs } from "firebase/firestore"
 import { onUnmounted } from "vue"
-import { getFirestore } from "firebase/firestore"
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_Q-aiMJggiHDCWNmbgIOHvMT3Bf1YWUc",
-  authDomain: "udemy-testing-2f637.firebaseapp.com",
-  projectId: "udemy-testing-2f637",
-  storageBucket: "udemy-testing-2f637.appspot.com",
-  messagingSenderId: "450783955581",
-  appId: "1:450783955581:web:6c41ae8d5299f92ab9bf14"
-};
+  apiKey: "AIzaSyCo7JdP7m6pQ_EWOHM9U6FR1a1_1gYe4O0",
+  authDomain: "vue-osem-salmon.firebaseapp.com",
+  projectId: "vue-osem-salmon",
+  storageBucket: "vue-osem-salmon.appspot.com",
+  messagingSenderId: "904724475427",
+  appId: "1:904724475427:web:44b0b070a8886574264b96"
+}
 
-
+//init firebase app
 const app = initializeApp(firebaseConfig)
+
+//init auth
 const auth = getAuth(app)
 
-const db = getFirestore(app)
+//init firestore
+const db = getFirestore()
 
+//collection reference
+const colRef = collection(db, 'patient')
 
-//const usersCollection = db.collection('users')
-
-
+//get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let patient = []
+    snapshot.docs.forEach((doc) => {
+      patient.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(patient)
+  })
+.catch(err => {
+  console.log(err.message)
+})
 
 export { auth }
-
-export const createUser = user => {
-  return usersCollection.add(user)
-}
-
-export const getUser = async id => {
-  const user = await usersCollection.doc(id).get()
-  return user.exist ? user.data() : null
-}
-
-export const updateUser = (id, user) => {
-  return usersCollection.doc(id).update(user)
-}
-
-export const deleteUser = id => {
-  return usersCollection.doc(id).delete()
-}
-
-export const useLoadUsers = () => {
-  const users = ref([])
-  usersCollection.onSnapshot(snapshot => {
-    users.value = snapshot.docs.map(doc => ({id: doc.id, ...doc.data() }))
-  })
-  onUnmounted(close)
-  return users
-}
--- src/views/addPatient.vue
+++ src/views/addPatient.vue
@@ -1,31 +1,20 @@
 <template>
-  <h1>add patient</h1>
-  <form @submit.prevent="onSubmit">
-    <input type="text" placeholder="Name" v-model="patient_form.name">
-    <input type="text" placeholder="NRIC" v-model="patient_form.nric">
-    <button type="submit">Add</button>
+  <h1>Add Patient</h1>
+
+  <form class="addpatient">
+    <label for="patientName"></label>
+    <input type="text" name="patientName" placeholder="Name" required>
+
+    <label for="patientIC"></label>
+    <input type="text" name="patientIC" placeholder="NRIC" required>
+
+    <button>Add Patient</button>
   </form>
+  
 </template>
 
 <script>
-import { createUser } from '@/firebase'
-import { reactive } from 'vue'
 
-export default {
-    setup() {
-        const patient_form = reactive({name: '', nric: ''})
-
-        const onSubmit = async () => {
-            await createUser({...patient_form})
-            patient_form.name = ''
-            patient_form.nric = ''
-        }
-
-        return {
-            patient_form, onSubmit
-        }
-    }
-}
 </script>
 
 <style>
