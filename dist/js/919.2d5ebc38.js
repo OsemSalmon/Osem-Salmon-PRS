7324dbe03d525307ee2564e23ff42b19d3726437
MUHAMAD ALIFF FAHMI BIN MOHAMAD ASRI <68102284+fahmi2000@users.noreply.github.com>
2022-06-18 16:14:21 +0800

    firebase 9 refactor

--- dist/js/919.2d5ebc38.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkappdev"]=self["webpackChunkappdev"]||[]).push([[919],{5919:function(n,u,e){e.r(u),e.d(u,{default:function(){return i}});var t=e(3396);const a={class:"about"},r=(0,t._)("h1",null,"About",-1),c=[r];function s(n,u){return(0,t.wg)(),(0,t.iD)("main",a,c)}var o=e(89);const p={},f=(0,o.Z)(p,[["render",s]]);var i=f}}]);
-//# sourceMappingURL=919.2d5ebc38.js.map
--- old/includes/signout-inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-echo "signed out";
-//header("location: ../index.html");
-exit();
