b15f7dac7fc1d009a2bd4d24f7bbaa8f806bf0fc
MUHAMAD ALIFF FAHMI BIN MOHAMAD ASRI <fahmialiff00@gmail.com>
2022-07-04 00:52:09 +0800

    ~user profile
    Co-Authored-By: Ariff_Iskandar <58976797+IsAlp@users.noreply.github.com>

--- src/views/userProfile.vue
+++ src/views/userProfile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" v-for="staff in staff" :key="staff.id">
     <div class="row">
       <div class="col">
         <h1 class="display-6">Profile</h1>
@@ -12,38 +12,42 @@
       <div class="col-3">
         <img src="https://via.placeholder.com/150" class="img-fluid" alt="">
       </div>
-      <div class="col" v-for="staff in staff" :key="staff.uid">
-        <form @submit.prevent>
+      <div class="col">
+        
+
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Name</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="staff.fullname">
+                <label class="form-label">User ID</label>
+                <input type="text" class="form-control" v-model="staff.uid" disabled>
+                <div class="form-text">You are not allowed to change this value.</div>
+            </div>
+        <form @submit.prevent="handleUpdate(staff)">
+            <div class="mb-3">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-control" v-model="staff.fullname">
             </div>
 
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">User ID</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="staff.uid">
-                <div id="emailHelp" class="form-text">You are not allowed to change this value.</div>
+                <label class="form-label">Contact Number</label>
+                <input type="text" class="form-control" v-model="staff.pNumber">
             </div>
 
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Contact Number</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="staff.pNumber">
-            </div>       
+                <button class="btn btn-primary float-end">Update</button>
+            </div>
         </form>
       </div>
-      <div class="col-3" v-for="staff in staff" :key="staff.uid">
+      <div class="col-3">
         <div class="mb-3">
-            
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+
+            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             Change Email
             </button>
 
-            <button type="button" class="btn btn-primary ms-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">
+            <button type="button" class="btn btn-secondary ms-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">
             Change Password
             </button>
 
 
-            <!-- Email Modal -->
             <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -62,7 +66,6 @@
             </div>
             </div>
 
-            <!-- Password Modal -->
             <div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel2" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
@@ -90,20 +93,32 @@
   </div>
 </template>
 
+
 <script>
 import { db } from "../firebase/config"
 import { doc, updateDoc } from "firebase/firestore"
+
 import getCollection from "../composables/getCollection"
 import getUserAuth from "../composables/getUserAuth"
 
 export default {
     setup () {
         const { userAuth } = getUserAuth()
+
         const { documents: staff } = getCollection('staff', 
         ['uid', '==', userAuth.value.uid])
 
+        const handleUpdate = (staff) => {
+            const docRef = doc(db, 'staff', staff.id)
+            updateDoc(docRef, {
+                fullname: staff.fullname,
+                pNumber: staff.pNumber,
+            })
+            alert('Profile updated')
+        }
+
         return {
-            staff, userAuth
+            staff, userAuth, handleUpdate
         }
     }
 }
